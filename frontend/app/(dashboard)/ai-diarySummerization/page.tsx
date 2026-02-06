a585d175c4eecb82a5388a9a69e1aa9eb7655b1f
Zynin43839 <Oranut_s@kkumail.com>
2026-02-06 17:16:27 +0700

    feat: install google genai sdk and add embedding logic for notes

--- frontend/app/(dashboard)/ai-diarySummerization/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function aiSummerizerPage() {
-  return (
-    <div>aiSummerizerPage</div>
-  )
-}
--- frontend/app/(dashboard)/ai-picGenerate/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function aiPictureGeneratePage() {
-  return (
-    <div>aiPictureGeneratePage</div>
-  )
-}
